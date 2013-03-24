c0abeecc364e394880a3ff0d7fcb15e6f73fb17b
Yury Velikanau <yury.velikanau@gmail.com>
2013-03-24 11:03:15 -0700

    Introduce user authentication (WIP).

--- config/application.rb
+++ config/application.rb
@@ -30,8 +30,8 @@ module Tracker
     config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -58,6 +58,9 @@ module Tracker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Deploying to Heroku requires this setting to be set.
+    config.assets.initialize_on_precompile = false
 
     # Automatically generate factories instead of fixtures.
     config.generators do |g|
