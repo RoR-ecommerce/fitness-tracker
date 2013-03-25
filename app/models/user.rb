90fd8bfc647e972dd028b35e9f820346decf6428
Yury Velikanau <yury.velikanau@gmail.com>
2013-03-24 17:00:02 -0700

    Protect all AR models from unpermitted params.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  include ActiveModel::ForbiddenAttributesProtection
-
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :validatable
 end
