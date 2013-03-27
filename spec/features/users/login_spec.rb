5280a80cd40263ed0c72422130948daa6a4e9999
Yury Velikanau <yury.velikanau@gmail.com>
2013-03-26 22:55:53 -0700

    Acceptance specs for * edit account * forgot password * login * logout * sign up
    "Welcome aboard" will be changed to something different once we have normal pages such as home page, etc.

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -1,19 +1,35 @@
 require 'spec_helper'
 
-describe '/users/login' do
+describe 'Login' do
   it 'displays home page after successful login' do
     user = FactoryGirl.create(:user)
 
-    visit   '/users/login'
-    fill_in 'Email',    with: user.email
-    fill_in 'Password', with: user.password
+    visit   new_user_session_path
+    fill_in 'user_email',    with: user.email
+    fill_in 'user_password', with: user.password
     click_button 'Sign in'
 
     expect(page).to have_text('Welcome aboard')
   end
 
   it 'displays error when no credential submitted' do
-    visit '/users/login'
+    visit new_user_session_path
+    click_button 'Sign in'
+
+    expect(page).to have_selector('#flash_alert', text: 'Invalid email or password.')
+  end
+
+  it 'displays error when email is missing' do
+    visit new_user_session_path
+    fill_in 'user_password', with: 'foobarbaz'
+    click_button 'Sign in'
+
+    expect(page).to have_selector('#flash_alert', text: 'Invalid email or password.')
+  end
+
+  it 'displays error when password is missing' do
+    visit new_user_session_path
+    fill_in 'user_email', with: 'foo@bar.baz'
     click_button 'Sign in'
 
     expect(page).to have_selector('#flash_alert', text: 'Invalid email or password.')
