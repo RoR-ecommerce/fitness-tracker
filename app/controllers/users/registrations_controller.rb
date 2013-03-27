941ed8a41e1c1570f016e37c2353d9121efc9dd9
Yury Velikanau <yury.velikanau@gmail.com>
2013-03-26 22:51:34 -0700

    :current_password also has to be whitelisted.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def resource_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
   end
 end
