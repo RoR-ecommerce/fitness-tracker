73538014954bb49b825b87289ebd644e95a427dc
Igor Pstyga <igor.pstyga@gmail.com>
2013-04-07 20:52:00 -0700

    Add more spec to user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,4 +35,25 @@ describe User do
       expect(FactoryGirl.build(:user)).to be_valid
     end
   end
+
+  describe '.from_omniauth!' do
+    before(:each) do
+      @existent_user = FactoryGirl.create(:user)
+    end
+
+    let(:user) { FactoryGirl.build(:user)}
+    let(:auth) { OmniAuth::AuthHash.new({
+                   provider: user.provider,
+                   uid: user.uid,
+                   info: { email: user.email} }) }
+
+    let(:auth2) { OmniAuth::AuthHash.new({
+                   provider: @existent_user.provider,
+                   uid: @existent_user.uid,
+                   info: { email: @existent_user.email} }) }
+
+    it { expect { User.from_omniauth!(auth) }.to change { User.count }.by(1) }
+    it { expect { User.from_omniauth!(auth2) }.to change { User.count }.by(0) }
+    it { expect(User.from_omniauth!(auth2)).to eq(@existent_user) }
+  end
 end
