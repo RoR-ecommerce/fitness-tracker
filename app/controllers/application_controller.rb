a21f3985a53c96bf73c02ebb0ba1b404f62a141c
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-09 21:08:58 -0700

    Merge apps: application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   ensure_security_headers
+
+  def route
+    if track = current_user.tracks.first
+      redirect_to track
+    else
+      redirect_to new_track_url
+    end
+  end
 end
