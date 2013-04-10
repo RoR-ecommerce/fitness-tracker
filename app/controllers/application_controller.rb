50a01230b16266708455d1d24294798acbb6ec75
Igor Pstyga <igor.pstyga@gmail.com>
2013-04-10 00:56:22 -0700

    Require authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   ensure_security_headers
 
+  before_filter :authenticate_user!
+
   def route
     if track = current_user.tracks.first
       redirect_to track
